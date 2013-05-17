23a4ae75e34ebcfbc62065c4ca37e36cefd6c0c0
Willy Aguirre <marti1125@gmail.com>
2013-05-17 12:39:35 -0500

    cambio de titulo por js

--- js/app.js
+++ js/app.js
@@ -1,50 +1,19 @@
 // buttons
-var btnHorario = document.querySelector("#horario-btn");
-var btnTwitter = document.querySelector("#twitter-btn");
-var btnInfo = document.querySelector("#info-btn");
+var btnHorario = document.getElementById("btn-horario");
+var btnTwitter = document.getElementById("btn-twitter");
+var btnInfo = document.getElementById("btn-info");
 
-// views
-var viewInicio = document.querySelector("#home-view");
-var viewHorario = document.querySelector("#horario-view");
-var viewTwitter = document.querySelector("#twitter-view");
-var viewInfo = document.querySelector("#info-view");
+btnHorario.onclick = function() {
+	var titulo = document.getElementById("hora").getAttribute("alt");
+	document.getElementById("titulo").innerHTML = titulo;
+};
 
-btnHorario.addEventListener('click', function () {
-	viewHorario.classList.remove('back-horario-view');
-	viewInicio.classList.add('back-inicio');	
-	viewHorario.classList.add('move-horario-view');
-});
+btnTwitter.onclick = function() {
+	var titulo = document.getElementById("twitter").getAttribute("alt");
+	document.getElementById("titulo").innerHTML = titulo;
+};
 
-btnTwitter.addEventListener('click', function() {
-	viewTwitter.classList.remove('back-twitter-view');
-	viewInicio.classList.add('back-inicio');	
-	viewTwitter.classList.add('move-twitter-view');
-});
-
-btnInfo.addEventListener('click', function() {
-	viewInfo.classList.remove('back-info-view');
-	viewInicio.classList.add('back-inicio');	
-	viewInfo.classList.add('move-info-view');
-});
-
-var btnback = document.querySelector("#back-btn");
-var btnback2 = document.querySelector("#back-btn2");
-var btnback3 = document.querySelector("#back-btn3");
-
-btnback.addEventListener ('click', function () {
-	viewInicio.classList.remove('back-inicio');
-	viewHorario.classList.remove('move-horario-view');	
-	viewHorario.classList.add('back-horario-view');
-});
-
-btnback2.addEventListener ('click', function () {
-	viewInicio.classList.remove('back-inicio');
-	viewTwitter.classList.remove('move-twitter-view');	
-	viewTwitter.classList.add('back-twitter-view');
-});
-
-btnback3.addEventListener ('click', function () {
-	viewInicio.classList.remove('back-inicio');
-	viewInfo.classList.remove('move-info-view');	
-	viewInfo.classList.add('back-info-view');
-});
+btnInfo.onclick = function() {
+	var titulo = document.getElementById("info").getAttribute("alt");
+	document.getElementById("titulo").innerHTML = titulo;
+};
