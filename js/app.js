209ce22ccd205ca37bee0e4d35059c75cb9813cf
Willy Aguirre <marti1003@hotmail.com>
2013-05-18 22:32:24 -0500

    se pone por defecto el tab horario

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 Zepto(function($){
 
+	$('#tabstart').addClass('active');
+
 	// Twitter
 	var twitterTimeLine = "https://api.twitter.com/1/statuses/user_timeline/Lineaunope.json?&count=7&callback=?";
 
@@ -10,14 +12,17 @@ Zepto(function($){
 	});
 
 	$('#btn-horario').click(function (){
+		$('#tabstart').removeClass('active');
 		$('#titulo').html($('#hora').attr('alt'));
 	});
 
 	$('#btn-twitter').click(function (){
+		$('#tabstart').removeClass('active');
 		$('#titulo').html($('#twitter').attr('alt'));
 	});
 
 	$('#btn-info').click(function (){
+		$('#tabstart').removeClass('active');
 		$('#titulo').html($('#info').attr('alt'));
 	});
 
