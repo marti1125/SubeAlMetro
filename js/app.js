e41858526b0d43361de8453c3a40560361697600
Willy Aguirre <marti1003@hotmail.com>
2013-05-18 20:15:16 -0500

    se actualiza el app.js

--- js/app.js
+++ js/app.js
@@ -9,27 +9,16 @@ Zepto(function($){
 		});
 	});
 
+	$('#btn-horario').click(function (){
+		$('#titulo').html($('#hora').attr('alt'));
+	});
+
+	$('#btn-twitter').click(function (){
+		$('#titulo').html($('#twitter').attr('alt'));
+	});
+
+	$('#btn-info').click(function (){
+		$('#titulo').html($('#info').attr('alt'));
+	});
+
 });
-
-// buttons
-var btnHorario = document.getElementById("btn-horario");
-var btnTwitter = document.getElementById("btn-twitter");
-var btnInfo = document.getElementById("btn-info");
-
-btnHorario.onclick = function() {
-	var titulo = document.getElementById("hora").getAttribute("alt");
-	document.getElementById("titulo").innerHTML = titulo;
-};
-
-btnTwitter.onclick = function() {
-	var titulo = document.getElementById("twitter").getAttribute("alt");
-	document.getElementById("titulo").innerHTML = titulo;
-};
-
-btnInfo.onclick = function() {
-	var titulo = document.getElementById("info").getAttribute("alt");
-	document.getElementById("titulo").innerHTML = titulo;
-};
-
-
-
