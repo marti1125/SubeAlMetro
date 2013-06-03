ac26a3dcfdfd981ed3863b900fa9f6b8a77f66f1
Willy Aguirre <marti1003@hotmail.com>
2013-06-02 20:41:35 -0500

    update

--- js/app.js
+++ js/app.js
@@ -35,3 +35,15 @@ Zepto(function($){
 	});
 
 });
+
+var dial = document.querySelector("#dial");
+if (dial) {
+dial.onclick = function () {
+  var call = new MozActivity({
+    name: "dial",
+    data: {
+    number: "0800-11121"
+  }
+  });
+}
+}
