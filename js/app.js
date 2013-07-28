fa6e002f22ac30a12ceb05ae5dcfa7fde431e991
Willy Aguirre <marti1125@gmail.com>
2013-07-28 03:14:20 -0500

    se agregan todas las estaciones

--- js/app.js
+++ js/app.js
@@ -45,14 +45,42 @@ Zepto(function($){
 		document.getElementById("info-mapa"), {
 			components: [
 			new nokia.maps.map.component.Behavior()],
-			zoomLevel: 10,
+			zoomLevel: 12,
 			center: [-12.050065023002462, -77.069091796875]
 	});
 
 	var ves = new nokia.maps.map.StandardMarker([-12.207415, -76.933404],{text: "1"});
 	var pi = new nokia.maps.map.StandardMarker([-12.196510, -76.939445],{text: "2"});
+	var pu = new nokia.maps.map.StandardMarker([-12.182321, -76.946890],{text: "3"});
+	var vm = new nokia.maps.map.StandardMarker([-12.169285, -76.950356],{text: "4"});
+	var ma = new nokia.maps.map.StandardMarker([-12.161188, -76.956332],{text: "5"});
+	var sj = new nokia.maps.map.StandardMarker([-12.156500, -76.965687],{text: "6"});
+	var at = new nokia.maps.map.StandardMarker([-12.150847, -76.979538],{text: "7"});
+	var jch = new nokia.maps.map.StandardMarker([-12.142587, -76.991015],{text: "8"});
+	var ay = new nokia.maps.map.StandardMarker([-12.134755, -76.997012],{text: "9"});
+	var ca = new nokia.maps.map.StandardMarker([-12.127667, -77.000669],{text: "10"});
+	var an = new nokia.maps.map.StandardMarker([-12.111075, -77.000208],{text: "11"});
+	var sbs = new nokia.maps.map.StandardMarker([-12.100967, -77.001694],{text: "12"});
+	var lc = new nokia.maps.map.StandardMarker([-12.087475, -77.003678],{text: "13"});
+	var na = new nokia.maps.map.StandardMarker([-12.076029, -77.010992],{text: "14"});
+	var ga = new nokia.maps.map.StandardMarker([-12.065137, -77.012411],{text: "15"});
+	var gr = new nokia.maps.map.StandardMarker([-12.055529, -77.013857],{text: "16"});		
 	map.objects.add(ves);
 	map.objects.add(pi);
+	map.objects.add(pu);
+	map.objects.add(vm);
+	map.objects.add(ma);
+	map.objects.add(sj);
+	map.objects.add(at);
+	map.objects.add(jch);
+	map.objects.add(ay);
+	map.objects.add(ca);
+	map.objects.add(an);
+	map.objects.add(sbs);
+	map.objects.add(lc);
+	map.objects.add(na);
+	map.objects.add(ga);
+	map.objects.add(gr);
 
 	// Acceso a internet
 	var xhr = new XMLHttpRequest({
