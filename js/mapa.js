6c729979c07b66ac2ac5981b764eaab7d06e62d7
Willy Aguirre <marti1125@gmail.com>
2013-08-05 17:00:27 -0500

    algunas correciones en mapas

--- js/mapa.js
+++ js/mapa.js
@@ -1,23 +1,22 @@
-/*nokia.Settings.set( "appId", "oXBdneZI8fw0fT9w6bmM");
-nokia.Settings.set( "authenticationToken", "TyIkck-eTNU2_0dmAxEX6A");
-
-var map = new nokia.maps.map.Display(
-		document.getElementById("info-mapa"), {
-		components: [
-			new nokia.maps.map.component.Behavior(),
-			new nokia.maps.map.component.ZoomBar(),
-			new nokia.maps.map.component.Overview()
-		],
-	zoomLevel: 12,
-	center: [-12.107227290349885, -76.99493408203125]
-});*/
-
 var coor_from = new OpenLayers.Projection("EPSG:4326");
 var coor_to   = new OpenLayers.Projection("EPSG:900913");
+
 map = new OpenLayers.Map("info-mapa");
+
+map.addControl(new OpenLayers.Control.PanZoomBar());
 map.addLayer(new OpenLayers.Layer.OSM());
 map.zoomToMaxExtent();
 var center    = new OpenLayers.LonLat(-77.0071244, -12.1160213);
 center.transform(coor_from, coor_to);
 map.addLayer(new OpenLayers.Layer.OSM());
 map.setCenter(center, 12);
+
+var markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
+
+// agregar markers!!
+var size = new OpenLayers.Size(40,45);
+var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+var ves = new OpenLayers.Icon('img/ves.png',size,offset);
+var vesPos = new OpenLayers.LonLat(-77.0071244, -12.1160213);
+markers.addMarker(new OpenLayers.Marker(vesPos.transform(coor_from, coor_to),ves));
