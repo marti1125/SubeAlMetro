56fc0e77c3b32df072b9d40fc972b72e40adab43
Willy Aguirre <marti1125@gmail.com>
2013-08-07 13:00:59 -0500

    correcion en los tamaños de los iconos

--- js/mapa.js
+++ js/mapa.js
@@ -1,7 +1,7 @@
-function agregarUbicacion(longitud, latitud, icon){
+function agregarUbicacion(longitud, latitud, icon, w, h){
 	var coor_from = new OpenLayers.Projection("EPSG:4326");
 	var coor_to   = new OpenLayers.Projection("EPSG:900913");
-	var size = new OpenLayers.Size(43,55);
+	var size = new OpenLayers.Size(w,h);
 	var offset = new OpenLayers.Pixel(-(size.w), -size.h);
 	var icon = new OpenLayers.Icon(icon,size,offset);
 	var mark = new OpenLayers.LonLat(longitud,latitud);
@@ -24,17 +24,10 @@ map.setCenter(center, 12);
 var markers = new OpenLayers.Layer.Markers( "Markers" );
 map.addLayer(markers);
 
-// agregar markers!!
-/*var size = new OpenLayers.Size(40,45);
-var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-var ves = new OpenLayers.Icon('img/ves.png',size,offset);
-var vesPos = new OpenLayers.LonLat(-77.0071244, -12.1160213);
-markers.addMarker(new OpenLayers.Marker(vesPos.transform(coor_from, coor_to),ves));*/
-
 $.getJSON('js/estaciones.json', function(response){
 	$.each(response, function(index, item){
 		$.each(item, function(index, result){
-			agregarUbicacion(result.longitud, result.latitud, result.icon)
+			agregarUbicacion(result.longitud, result.latitud, result.icon, result.w, result.h)
 		});			  
 	});
 });
