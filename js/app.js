6ee88db721bd131badb8e9471d49c16d450c5e32
Willy Aguirre <marti1125@gmail.com>
2013-08-21 14:10:32 -0500

    cambia de color el icono del tab seleccionado

--- js/app.js
+++ js/app.js
@@ -39,7 +39,12 @@ function btnEvents(btnName){
 	$('#btn-' + btnName).click(function (){
 		$('#settings-view').removeClass('bajar');
 		$('.active').removeClass('active');		
-		$('#titulo').html($('#' + btnName).attr('alt'));		
+		$('#titulo').html($('#' + btnName).attr('alt'));
+        $('#estacion').attr('src','img/estacion.png');
+        $('#mapa').attr('src','img/mapa.png');
+        $('#twitter').attr('src','img/twitter.png');
+        $('#info').attr('src','img/info.png');
+        $('#'+btnName).attr('src','img/'+btnName+'2.png');
 	});
 }
 
@@ -79,7 +84,8 @@ $(document).ready(function(){
 	    mozSystem: true
 	});
 
-	$('.active').addClass('active');	
+	$('.active').addClass('active');
+    $('#estacion').attr('src','img/estacion2.png');
 
   	var buttons = ['estacion', 'mapa', 'twitter', 'info'];
   		$.map(buttons, function(button){
